875ca8c1bbc146be5020815623bcbc5da7b30c1a
Андреев Александр <aandreev171202@gmail.com>
2026-08-30 05:20:05 +0300

    creating db schema

---
+++ feature/chat/database/src/commonMain/kotlin/ru/missclick/chat/database/entities/ChatEntity.kt
@@ -0,0 +1,11 @@
+package ru.missclick.chat.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ChatEntity(
+    @PrimaryKey val chatId: String,
+    val lastMessage: String?,
+    val lastActivityAt: Long
+)
