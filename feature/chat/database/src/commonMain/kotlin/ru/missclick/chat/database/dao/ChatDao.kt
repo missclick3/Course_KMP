2196d41a30d0c81796fb4034fb5cc43cafcd6e60
Андреев Александр <aandreev171202@gmail.com>
2026-08-30 05:37:41 +0300

    chat dao

---
+++ feature/chat/database/src/commonMain/kotlin/ru/missclick/chat/database/dao/ChatDao.kt
@@ -0,0 +1,59 @@
+package ru.missclick.chat.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+import ru.missclick.chat.database.entities.ChatEntity
+import ru.missclick.chat.database.entities.ChatInfoEntity
+import ru.missclick.chat.database.entities.ChatParticipantEntity
+import ru.missclick.chat.database.entities.ChatWithParticipants
+
+@Dao
+interface ChatDao {
+
+    @Upsert
+    suspend fun upsertChat(chat: ChatEntity)
+
+    @Upsert
+    suspend fun upsertChats(chats: List<ChatEntity>)
+
+    @Query("DELETE FROM chatentity WHERE chatId = :chatId")
+    suspend fun deleteChatById(chatId: String)
+
+    @Query("SELECT * FROM chatentity ORDER BY lastActivityAt DESC")
+    fun getChatsWithParticipants(): Flow<ChatWithParticipants>
+
+    @Query("SELECT * FROM chatentity WHERE chatId = :id")
+    suspend fun getChatById(id: String): ChatWithParticipants
+
+    @Query("DELETE FROM chatentity")
+    suspend fun deleteAllChat()
+
+    @Query("SELECT chatId FROM chatentity")
+    suspend fun getAllChatIds(): List<String>
+
+    @Transaction
+    suspend fun deleteChatsByIds(chatIds: List<String>) {
+        chatIds.forEach { chatId ->
+            deleteChatById(chatId)
+        }
+    }
+
+    @Query("SELECT COUNT(*) FROM chatentity")
+    fun getChatCount(): Flow<Int>
+
+    @Query("""
+        SELECT p.*
+        FROM chatparticipantentity p
+        JOIN chatparticipantcrossref cpcr ON p.userId = cpcr.userId
+        WHERE cpcr.chatId = :chatId AND cpcr.isActive = true
+        ORDER BY p.username
+    """)
+    fun getActiveParticipantsByChatId(chatId: String): Flow<List<ChatParticipantEntity>>
+
+    @Query("SELECT * FROM chatentity WHERE chatId = :chatId")
+    fun getChatInfoById(chatId: String): Flow<ChatInfoEntity>
+}
