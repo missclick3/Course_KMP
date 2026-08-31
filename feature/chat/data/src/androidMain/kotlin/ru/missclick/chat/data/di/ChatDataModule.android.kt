47fc0e78e7cde78a2b5e1c1db1f3e7be670368f8
Андреев Александр <aandreev171202@gmail.com>
2026-08-31 17:29:05 +0300

    connectivity observer

--- feature/chat/data/src/androidMain/kotlin/ru/missclick/chat/data/di/ChatDataModule.android.kt
+++ feature/chat/data/src/androidMain/kotlin/ru/missclick/chat/data/di/ChatDataModule.android.kt
@@ -4,9 +4,11 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import ru.missclick.chat.data.lifecycle.AppLifecycleObserver
+import ru.missclick.chat.data.network.ConnectivityObserver
 import ru.missclick.chat.database.DatabaseFactory
 
 actual val platformChatDataModule = module {
     single { DatabaseFactory(androidContext()) }
     singleOf(::AppLifecycleObserver)
+    singleOf(::ConnectivityObserver)
 }
--- feature/chat/data/src/iosMain/kotlin/ru/missclick/chat/data/di/ChatDataModule.ios.kt
+++ feature/chat/data/src/iosMain/kotlin/ru/missclick/chat/data/di/ChatDataModule.ios.kt
@@ -3,9 +3,11 @@ package ru.missclick.chat.data.di
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import ru.missclick.chat.data.lifecycle.AppLifecycleObserver
+import ru.missclick.chat.data.network.ConnectivityObserver
 import ru.missclick.chat.database.DatabaseFactory
 
 actual val platformChatDataModule = module {
     single { DatabaseFactory() }
     singleOf(::AppLifecycleObserver)
+    singleOf(::ConnectivityObserver)
 }
