4c764c9cbd0f9c39fd46d32683780d85db639418
Андреев Александр <aandreev171202@gmail.com>
2026-08-31 19:57:25 +0300

    connection retry handler

---
+++ feature/chat/data/src/commonMain/kotlin/ru/missclick/chat/data/network/ConnectionRetryHandler.kt
@@ -0,0 +1,34 @@
+package ru.missclick.chat.data.network
+
+import kotlinx.coroutines.delay
+import kotlin.math.pow
+import kotlin.time.Duration.Companion.milliseconds
+
+class ConnectionRetryHandler(
+    private val connectionErrorHandler: ConnectionErrorHandler
+) {
+    private var shouldSkipBackoff = false
+
+    fun shouldRetry(cause: Throwable, attempt: Long): Boolean {
+        return connectionErrorHandler.isRetriableError(cause)
+    }
+
+    suspend fun applyRetryDelay(attempt: Long) {
+        if (!shouldSkipBackoff) {
+            val delay = createBackoffDelay(attempt)
+            delay(delay.milliseconds)
+        } else {
+            shouldSkipBackoff = false
+        }
+    }
+
+    fun resetDelay() {
+        shouldSkipBackoff = true
+    }
+
+    private fun createBackoffDelay(attempt: Long): Long {
+        val delayTime = (2f.pow(attempt.toInt()) * 2000).toLong()
+        val maxDelay = 30_000L
+        return minOf(delayTime, maxDelay)
+    }
+}
